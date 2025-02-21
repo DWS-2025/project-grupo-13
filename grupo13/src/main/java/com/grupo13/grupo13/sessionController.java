1f6ba31b28ecc921f9c3a65e9570452b1a07c870
“praton2023” <p.raton.2023@alumnos.urjc.es>
2025-02-21 12:43:38 +0100

    añadido lista de armaduras y armas, e imagenes

--- grupo13/src/main/java/com/grupo13/grupo13/sessionController.java
+++ grupo13/src/main/java/com/grupo13/grupo13/sessionController.java
@@ -54,11 +54,96 @@ public class sessionController {
     @GetMapping("/list_objects")
 	public String iterationObj(Model model) {
 
-		List<Person> people = new ArrayList<>();
+		List<Armor> armorList = new ArrayList<>();
+
+        
+        armorList.add(new Armor("Wood Armor", 20, 
+            "https://dummyimage.com/320x240/87f7ff/000000.png&text=Wood+Armor", 
+            "Obtained from an oak tree. Light but fragile."));
+
+        armorList.add(new Armor("Iron Plate", 50, 
+            "https://dummyimage.com/320x240/a8a8a8/000000.png&text=Iron+Plate", 
+            "A strong iron armor that provides good protection."));
+
+        armorList.add(new Armor("Golden Chestplate", 40, 
+            "https://dummyimage.com/320x240/ffd700/000000.png&text=Golden+Chestplate", 
+            "Made of gold. Shiny but weak."));
+
+        armorList.add(new Armor("Dragon Scale Armor", 80, 
+            "https://dummyimage.com/320x240/dc143c/000000.png&text=Dragon+Scale", 
+            "Forged from the scales of an ancient dragon."));
+
+        armorList.add(new Armor("Shadow Cloak", 25, 
+            "https://dummyimage.com/320x240/1a1a1a/ffffff.png&text=Shadow+Cloak", 
+            "A mysterious cloak that helps the wearer move unseen."));
+
+        armorList.add(new Armor("Crystal Armor", 65, 
+            "https://dummyimage.com/320x240/87ceeb/000000.png&text=Crystal+Armor", 
+            "A magical armor made of enchanted crystals."));
+
+        armorList.add(new Armor("Obsidian Plate", 90, 
+            "https://dummyimage.com/320x240/2c2c2c/ffffff.png&text=Obsidian+Plate", 
+            "Extremely tough armor, made from volcanic obsidian."));
+
+        armorList.add(new Armor("Elven Leather", 35, 
+            "https://dummyimage.com/320x240/6b8e23/000000.png&text=Elven+Leather", 
+            "Lightweight and flexible, used by elven warriors."));
+
+        armorList.add(new Armor("Demonic Armor", 95, 
+            "https://dummyimage.com/320x240/8b0000/ffffff.png&text=Demonic+Armor", 
+            "An armor cursed with demonic energy, extremely durable."));
+
+        armorList.add(new Armor("Celestial Robes", 55, 
+            "https://dummyimage.com/320x240/ffffff/000000.png&text=Celestial+Robes", 
+            "Blessed by the gods, grants divine protection."));
 		
 
-		model.addAttribute("people", people);
+		model.addAttribute("armors", armorList);
 
+        List<Weapon> weaponList = new ArrayList<>();
+
+      
+        weaponList.add(new Weapon("Wood Sword", 20, 
+            "https://i.imgur.com/ZJDJakr.jpeg", 
+            "A basic sword made from oak wood."));
+
+        weaponList.add(new Weapon("Iron Longsword", 50, 
+            "https://i.imgur.com/xq1BDBK.jpeg", 
+            "A strong and reliable iron longsword."));
+
+        weaponList.add(new Weapon("Golden Rapier", 40, 
+            "https://i.imgur.com/Bm5NeRx.jpeg", 
+            "A golden rapier. Stylish but fragile."));
+
+        weaponList.add(new Weapon("Dragon Fang Blade", 80, 
+            "https://dummyimage.com/320x240/dc143c/000000.png&text=Dragon+Fang", 
+            "A blade crafted from the fang of an ancient dragon."));
+
+        weaponList.add(new Weapon("Shadow Dagger", 25, 
+            "https://dummyimage.com/320x240/1a1a1a/ffffff.png&text=Shadow+Dagger", 
+            "A dagger infused with dark energy, perfect for stealth attacks."));
+
+        weaponList.add(new Weapon("Crystal Spear", 65, 
+            "https://dummyimage.com/320x240/87ceeb/000000.png&text=Crystal+Spear", 
+            "A magical spear made from enchanted crystals."));
+
+        weaponList.add(new Weapon("Obsidian War Axe", 90, 
+            "https://dummyimage.com/320x240/2c2c2c/ffffff.png&text=Obsidian+Axe", 
+            "A massive axe forged from volcanic obsidian."));
+
+        weaponList.add(new Weapon("Elven Bow", 35, 
+            "https://dummyimage.com/320x240/6b8e23/000000.png&text=Elven+Bow", 
+            "A lightweight bow used by elven hunters."));
+
+        weaponList.add(new Weapon("Demonic Scythe", 95, 
+            "https://dummyimage.com/320x240/8b0000/ffffff.png&text=Demonic+Scythe", 
+            "A cursed scythe imbued with demonic power."));
+
+        weaponList.add(new Weapon("Celestial Staff", 55, 
+            "https://dummyimage.com/320x240/ffffff/000000.png&text=Celestial+Staff", 
+            "A holy staff blessed by the gods, channeling divine energy."));
+
+            model.addAttribute("weapons", weaponList);
 		return "listing";
 	}
 
