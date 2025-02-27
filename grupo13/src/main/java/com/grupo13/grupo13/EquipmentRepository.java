05cf6413bb46169d85cd3ca14327b0111a1357e9
LotusLupe <l.lotti.2023@alumnos.urjc.es>
2025-02-27 12:31:51 +0100

    Arreglar problema de conflicto

--- grupo13/src/main/java/com/grupo13/grupo13/EquipmentRepository.java
+++ grupo13/src/main/java/com/grupo13/grupo13/EquipmentRepository.java
@@ -1,53 +1,4 @@
 package com.grupo13.grupo13;
-<<<<<<< HEAD
-
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
-
-import javax.swing.plaf.TreeUI;
-
-import org.springframework.stereotype.Component;
-
-
-
-@Component
-public class EquipmentRepository{
-
-    private AtomicLong nextId = new AtomicLong(1L);
-	private ConcurrentHashMap<Long, Equipment> equip = new ConcurrentHashMap<>();
-
-    public List<Equipment> findAll() {
-        return equip.values().stream().toList();
-    }
-
-public Boolean isArmor(Equipment e){
-
-return e instanceof Armor;
-
-}
-
-public Boolean isWeapon(Equipment e){
-
-    return e instanceof Weapon;
-    
-    }
-
-
-
-}
-
-  /*   public void save(User user) {
-        long id = user.getId();
-        if (id == 0) {
-            id = nextId.getAndIncrement();
-            user.setId(id);
-        }
-        users.put(id, user);
-    }
-
-*/
-=======
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -81,4 +32,3 @@ public class EquipmentRepository {
         equipments.remove(id);
     }
 }
->>>>>>> Lotti
