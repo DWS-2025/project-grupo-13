de38ac75a2b280ec4555af32739d3b2effa6de3e
“dreamrcurio” <zulito33@gmail.com>
2025-03-11 10:14:51 +0100

    Modificado dependencias para trabajar con h2

--- grupo13/src/main/java/com/grupo13/grupo13/controller/sessionController.java
+++ grupo13/src/main/java/com/grupo13/grupo13/controller/sessionController.java
@@ -25,7 +25,6 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
