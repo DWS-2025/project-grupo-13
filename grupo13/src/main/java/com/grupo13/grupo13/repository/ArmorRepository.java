fe6baf1b74433ecc5873069e8e676ad056b956e4
LotusLupe <l.lotti.2023@alumnos.urjc.es>
2025-03-21 11:14:59 +0100

    imported jpa in armor repository

--- grupo13/src/main/java/com/grupo13/grupo13/repository/ArmorRepository.java
+++ grupo13/src/main/java/com/grupo13/grupo13/repository/ArmorRepository.java
@@ -3,6 +3,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import com.grupo13.grupo13.model.Armor;
 
