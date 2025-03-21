64e58df21c6e14a2c64b9856a6f94d0e20a3b17d
“praton2023” <p.raton.2023@alumnos.urjc.es>
2025-03-21 09:49:11 +0100

    actualizando session controller

--- grupo13/src/main/java/com/grupo13/grupo13/controller/sessionController.java
+++ grupo13/src/main/java/com/grupo13/grupo13/controller/sessionController.java
@@ -17,7 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import com.grupo13.grupo13.model.Character;
-import com.grupo13.grupo13.model.Equipment;
+import com.grupo13.grupo13.model.Weapon;
+import com.grupo13.grupo13.model.Armor;
 import com.grupo13.grupo13.service.CharacterService;
 import com.grupo13.grupo13.service.EquipmentService;
 import com.grupo13.grupo13.service.UserService;
@@ -25,6 +26,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
@@ -42,12 +44,14 @@ public class sessionController {
     @GetMapping("/")
     public String index(Model model, HttpSession session) {
 
-        ArrayList<Equipment> currentInventory = userService.currentUserInventory();
+        ArrayList<Weapon> currentInventoryWeapon = userService.currentUserInventoryWeapon();
+        ArrayList<Armor> currentInventoryArmor = userService.currentUserInventoryArmor();
         Character character = userService.getCharacter();
 
         //gets the character and its inventory for mustache
         model.addAttribute("character", character);
-        model.addAttribute("current", currentInventory);
+        model.addAttribute("currentW", currentInventoryWeapon);
+        model.addAttribute("currentA", currentInventoryWeapon);
         model.addAttribute("user", userService.getLoggedUser());
 
 
@@ -85,8 +89,10 @@ public class sessionController {
         characterImage.transferTo(imagePath);
 
         model.addAttribute("character", character);
-        ArrayList<Equipment> currentInventory = userService.currentUserInventory();
-        model.addAttribute("current", currentInventory);
+        ArrayList<Weapon> currentWeapon = userService.currentUserInventoryWeapon();
+        ArrayList<Armor> currentArmor = userService.currentUserInventoryArmor();
+        model.addAttribute("currentW", currentWeapon);
+        model.addAttribute("currentA", currentArmor);
         model.addAttribute("user", userService.getLoggedUser());
 
         return "character_view";
@@ -95,33 +101,61 @@ public class sessionController {
     @GetMapping("/list_objects")
     public String iterationObj(Model model, HttpSession session) {
 
-        List<Equipment> equipmentList = equipmentService.findAll();
-        ArrayList<Equipment> available = new ArrayList<>();
-        ArrayList<Equipment> currentInventory = userService.currentUserInventory();
+        List<Weapon> equipmentListW = equipmentService.findAllWeapon();
+        List<Armor> equipmentListA = equipmentService.findAllArmor();
+        ArrayList<Weapon> availableW = new ArrayList<>();
+        ArrayList<Armor> availableA = new ArrayList<>();
+        ArrayList<Weapon> currentInventoryW = userService.currentUserInventoryWeapon();
+        ArrayList<Armor> currentInventoryA = userService.currentUserInventoryArmor();
 
         //gets the listing of the current equipment in the repository and the inventory
         //creates a list of the equipment that the user doesnt have
         //the html will present the inventory as purchased and the not purchased (available) equipments
-        for (Equipment equipment : equipmentList) {
-            if (!currentInventory.contains(equipment)) {
-                available.add(equipment);
+        for (Weapon equipmentW : equipmentListW) {
+            if (!currentInventoryW.contains(equipmentW)) {
+                availableW.add(equipmentW);
+            }
+        }
+        for (Armor equipmentA : equipmentListA) {
+            if (!currentInventoryA.contains(equipmentA)) {
+                availableA.add(equipmentA);
             }
         }
         model.addAttribute("user", userService.getLoggedUser());
-        model.addAttribute("current", currentInventory);
-        model.addAttribute("available", available);
+        model.addAttribute("currentA", currentInventoryA);
+        model.addAttribute("currentW", currentInventoryW);
+        model.addAttribute("availableA", availableA);
+        model.addAttribute("availableW", availableW);
         return "listing";
     }
 
-    @PostMapping("/purchase")
-    public String purchase(@RequestParam long id, Model model) {
+    @PostMapping("/purchaseWeapon")
+    public String purchaseWeapon(@RequestParam long id, Model model) {
 
-        Optional<Equipment> eqOptional = equipmentService.findById(id);
+        Optional<Weapon> eqOptional = equipmentService.findWeaponById(id);
         if (eqOptional.isPresent()) {
             //checks if the user has enough money or if it's homeless
             int money = userService.getMoney();
             if (money >= eqOptional.get().getPrice()) {
-                userService.saveEquipment(id);
+                userService.saveWeapon(id);
+                return "redirect:/list_objects";
+            } else{
+                model.addAttribute("message", "You don't have any money left, go work or something");
+                return "sp_errors";
+            }
+        }
+        model.addAttribute("message", "Could not purchase, doesnt exist");
+        return "sp_errors";
+    }
+    @PostMapping("/purchaseArmor")
+    public String purchaseArmor(@RequestParam long id, Model model) {
+
+        Optional<Armor> eqOptional = equipmentService.findArmorById(id);
+        if (eqOptional.isPresent()) {
+            //checks if the user has enough money or if it's homeless
+            int money = userService.getMoney();
+            if (money >= eqOptional.get().getPrice()) {
+                userService.saveArmor(id);
                 return "redirect:/list_objects";
             } else{
                 model.addAttribute("message", "You don't have any money left, go work or something");
@@ -155,18 +189,33 @@ public class sessionController {
                 .body(image);
     }
 
-    @PostMapping("/equip")
-    public String equip(@RequestParam long id, Model model) {
+    @PostMapping("/equipWeapon")
+    public String equipWeapon(@RequestParam long id, Model model) {
         
         Character character = userService.getCharacter();
-        Optional<Equipment> equipment = equipmentService.findById(id);
+        Optional<Weapon> equipment = equipmentService.findWeaponById(id);
 
         if(equipment.isPresent()){ //if it exists
-            if(equipmentService.isWeapon(equipment.get())){ //checks if its a weapon or an armor
+        
                 characterService.equipWeapon(equipment.get(), character); //equips it, adding the necessary attributes
-            } else{
-                characterService.equipArmor(equipment.get(), character);
-            }        
+            
+            return "redirect:/";
+
+        }else{
+            model.addAttribute("message", "Could not equip, doesnt exist");
+            return "sp_errors";
+        }
+    }
+    @PostMapping("/equipArmor")
+    public String equipArmor(@RequestParam long id, Model model) {
+        
+        Character character = userService.getCharacter();
+        Optional<Armor> equipment = equipmentService.findArmorById(id);
+
+        if(equipment.isPresent()){ //if it exists
+        
+                characterService.equipArmor(equipment.get(), character); //equips it, adding the necessary attributes
+            
             return "redirect:/";
 
         }else{
@@ -175,18 +224,33 @@ public class sessionController {
         }
     }
 
-    @PostMapping("/unEquip")
-    public String unEquip(@RequestParam long id, Model model) {
+    @PostMapping("/unEquipWeapon")
+    public String unEquipWeapon(@RequestParam long id, Model model) {
         
         Character character = userService.getCharacter();
-        Optional<Equipment> equipment = equipmentService.findById(id);
+        Optional<Weapon> equipment = equipmentService.findWeaponById(id);
 
         if(equipment.isPresent()){
-            if(equipmentService.isWeapon(equipment.get())){ //same checks as last, this time hust sets the attributes o null or 0
-                characterService.unEquipWeapon(character, id);
-            } else{
-                characterService.unEquipArmor( character, id);
-            }        
+      
+        characterService.unEquipWeapon(character, id);
+               
+            return "redirect:/";
+
+        }else{
+            model.addAttribute("message", "Could not unEquip, doesnt exist");
+            return "sp_errors";
+        }
+    }
+    @PostMapping("/unEquipArmor")
+    public String unEquipArmor(@RequestParam long id, Model model) {
+        
+        Character character = userService.getCharacter();
+        Optional<Armor> equipment = equipmentService.findArmorById(id);
+
+        if(equipment.isPresent()){
+      
+        characterService.unEquipArmor(character, id);
+               
             return "redirect:/";
 
         }else{
