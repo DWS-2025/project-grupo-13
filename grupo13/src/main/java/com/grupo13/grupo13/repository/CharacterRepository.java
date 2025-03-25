a97a5d0cae032b248f514a4b13bf31bd3815d4c8
“praton2023” <p.raton.2023@alumnos.urjc.es>
2025-03-25 10:15:51 +0100

    consultas basicas

--- grupo13/src/main/java/com/grupo13/grupo13/repository/CharacterRepository.java
+++ grupo13/src/main/java/com/grupo13/grupo13/repository/CharacterRepository.java
@@ -1,9 +1,22 @@
 package com.grupo13.grupo13.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.grupo13.grupo13.model.Character;
+import com.grupo13.grupo13.model.User;
 
 
 public interface CharacterRepository  extends JpaRepository<Character, Long>{
+    @Query("SELECT c FROM Character c WHERE c.weapon.name = :weaponName")
+    List<Character> findCharactersByWeaponName(@Param("weaponName") String weaponName);
+    @Query("SELECT c FROM Character c WHERE c.armor.name = :armorName")
+    List<Character> findCharactersByArmorName(@Param("armorName") String armorName);
+
+
+
 
     /* 
     //attributes
