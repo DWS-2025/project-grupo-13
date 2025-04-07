90522867b888d6dbcfcb4b0c59fc89c40e24f772
“praton2023” <p.raton.2023@alumnos.urjc.es>
2025-04-07 21:51:15 +0200

    eliminado import intecesario

--- grupo13/src/main/java/com/grupo13/grupo13/controller/grupo13RestController.java
+++ grupo13/src/main/java/com/grupo13/grupo13/controller/grupo13RestController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.grupo13.grupo13.dto.WeaponDTO;
+
 
 import com.grupo13.grupo13.model.Armor;
 import com.grupo13.grupo13.model.Weapon;
